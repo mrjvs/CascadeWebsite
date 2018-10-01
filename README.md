fb4ee1f6e732a7de61b19ed787def650f12656e8
mrjvs <jellevs@gmail.com>
2018-10-01 22:29:09 +0200

    added basic express typescript code structure + readme file

---
+++ README.md
@@ -0,0 +1,4 @@
+# cascadebot website
+
+written in typescript.
+it includes user panel for bot customisations and normal website for docs
---
+++ src/utils/db.ts
@@ -0,0 +1,15 @@
+import Mongoose from "mongoose";
+const connectionString: string = process.env.DB_STRING  || "";
+
+if (connectionString === "") {
+    throw new Error("No connectionString plz fix");
+}
+
+let Connection: Mongoose.Connection = new Mongoose.Connection(Mongoose);
+export const init = () => {
+    Mongoose.connect(connectionString);
+    Connection = Mongoose.connection;
+    Connection.on("error", console.error.bind(console, "connection error:"));
+};
+
+export default Connection;
