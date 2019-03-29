90b9fd97749e9a7d57cae274f6d62679e23bb411
mrjvs <jellevs@gmail.com>
2019-03-29 12:59:29 +0100

    added subdomain support

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,18 +1,19 @@
 import express from "express";
 import { Request, Response } from "express";
 import cookieParser from "cookie-parser";
+import vhost from "vhost";
 
 import websiteRouter from "./routes/website";
 import dashboardRouter from "./routes/dashboard";
 
-import { port } from "./config.json";
+import { port, domains } from "./config.json";
 
 const app: express.Application = express();
 app.use(cookieParser());
 app.use(express.urlencoded());
 
-// app.use(websiteRouter);
-app.use(dashboardRouter);
+app.use(vhost(domains.website, websiteRouter));
+app.use(vhost(domains.dashboard, dashboardRouter));
 
 app.listen(port);
 console.log("Running on port " + port);
