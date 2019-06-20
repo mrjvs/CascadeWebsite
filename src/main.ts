bce3d59b3a206d2f24ac124eeba7db6ebe612a75
mrjvs <jellevs@gmail.com>
2019-06-20 16:20:23 +0200

    fixed formatting issue

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import websiteRouter from "./routes/website";
 const app: express.Application = express();
 app.use(websiteRouter);
 
-const port : number = Number(process.env.PORT) || 8080;
+const port: number = Number(process.env.PORT) || 8080;
 
 app.listen(port);
 console.log(`Running on port ${port}`);
